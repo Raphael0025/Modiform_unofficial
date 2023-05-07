a7a8e088454e58016fd7d30d8eaf67f1e85a56d3
Raphael Isla <rpbisla0025@gmail.com>
2023-05-08 03:32:44 +0800

    4 pages to go

--- js/script.js
+++ js/script.js
@@ -1,3 +1,18 @@
 function move(){
     window.location.href="index.html"
 }
+
+function openNav() {
+    var target = document.getElementById("side");
+    var target2 = document.getElementById("side2");
+    target.style.left = "0px";
+    target2.style.display = "block";
+    target2.style.animation = "fade-in 1s";
+}
+
+function closeNav() {
+    var target = document.getElementById("side");
+    var target2 = document.getElementById("side2");
+    target.style.left = "-430px";
+    target2.style.display = "none";
+}
